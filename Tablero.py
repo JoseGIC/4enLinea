b8175d8b9f5d870327a76190134354ae236e252d
JoseGIC <jm.perezo@alumnos.urjc.es>
2018-04-14 20:22:43 +0200

    Ya comprueba filas y columnas bien. Falta comprobar las diagonales

--- Tablero.py
+++ Tablero.py
@@ -32,6 +32,15 @@ class Tablero:
     def setMatriz(self, matriz):
         self.matriz = matriz
 
+    def getFila(self, numFila):
+        return self.matriz[numFila - 1]
+
+    def getColumna(self, numColumna):
+        columna = []
+        for i in range(self.numFilas):
+            columna.append(self.matriz[i][numColumna - 1])
+        return columna
+
 
     def pintarse(self):
         for i in range(self.numFilas):
@@ -45,26 +54,54 @@ class Tablero:
             print
         print
 
-    def mostrarse (self):
+    def mostrarse(self):
         for i in range(self.numFilas):
             print self.matriz[i]
 
-    def ponerFicha(self, columna, ficha):
-        for i in reversed (range(self.numFilas)):
-            if self.matriz[i][columna - 1] == 0:
-                self.matriz[i][columna - 1] = ficha
+
+    def ponerFicha(self, numColumna, ficha):
+        for i in reversed(range(self.numFilas)):
+            if self.matriz[i][numColumna - 1] == 0:
+                self.matriz[i][numColumna - 1] = ficha
+                self.pintarse()
+                self.comprobarLineas(i + 1, numColumna, ficha)
                 break
 
+    def comprobarLineas(self, numFila, numColumna, ficha):
+        fila = self.getFila(numFila)
+        columna = self.getColumna(numColumna)
+        #diagonalDcha = ...
+        #diagonaIzda = ...
+        linea = self.lineaHecha(fila, ficha) or \
+                self.lineaHecha(columna, ficha) #or \
+                #self.lineaHecha(diagonalDcha, ficha) or \
+                #self.lineaHecha(diagonaIzda, ficha)
+        if linea:
+            print "VICTORIA!"
+
+    def lineaHecha(self, linea, ficha):
+        total = 0
+        for i in linea:
+            if i == ficha:
+                total += 1
+                if total == 4:
+                    return True
+            else:
+                total = 0
+        return False
+
+    def diagonalHecha(self, fila, columna, ficha):
+        pass
+
+
 t1 = Tablero()
-t1.pintarse()
 t1.ponerFicha(4, 1)
-t1.pintarse()
 t1.ponerFicha(2, 1)
-t1.pintarse()
 t1.ponerFicha(2, 2)
-t1.pintarse()
 t1.ponerFicha(3, 2)
-t1.pintarse()
 t1.ponerFicha(2, 2)
-t1.pintarse()
-t1.mostrarse()
+t1.ponerFicha(5, 1)
+t1.ponerFicha(6, 1)
+#t1.ponerFicha(7, 1)
+t1.ponerFicha(2, 2)
+t1.ponerFicha(2, 2)
